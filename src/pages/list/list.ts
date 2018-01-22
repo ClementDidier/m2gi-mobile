bacf31f50da5a8d64b82df5496d5b889c21dae5a
Clément D <didiercl@outlook.fr>
2018-01-22 15:37:41 +0100

    Updating listpage contents

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,26 +1,38 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, IonicModule } from 'ionic-angular';
+import { TodoList } from '../../model/todo-list.model';
+import { TodoProvider } from '../../providers/todo/todo';
 
 /**
- * Generated class for the ListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the ListPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html',
+    selector: 'page-list',
+    templateUrl: 'list.html',
 })
 export class ListPage {
-  listId : string;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.listId = navParams.get('id');
-  }
+    list: TodoList;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ListPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, public todoService: TodoProvider) {
+        var uuid = this.navParams.get('id');
+        this.list = {
+            uuid: uuid,
+            name: 'listName',
+            items: []
+        };
+        this.todoService.getTodos(uuid).subscribe(todoItems => this.list.items = todoItems);
+    }
 
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ListPage');
+    }
+
+    deleteItem() {
+        console.log('');
+    }
 }
