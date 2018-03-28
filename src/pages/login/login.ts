070df59aa5a1149ea7e1562b90cb6abd641ec092
Clément D <didiercl@outlook.fr>
2018-03-28 12:41:54 +0200

    Ajout de la reinitialisation mot de passe

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -67,7 +67,31 @@ export class LoginPage {
         }
     }
 
+    /**
+     * Déconnecte la session courante
+     */
     public logOut(): void {
         this.logger.logOut();
+    }
+
+    /**
+     * Envoi une demande de confirmation pour reinitialiser le mot de passe
+     */
+    public forgotPassword(): void {
+        if(this.email && this.email.length > 0) {
+            this.logger.resetPassword(this.email).then(() => {
+                this.alert.create({
+                    title: this.translate.instant('reset-ok-modal-title'),
+                    subTitle: this.translate.instant('reset-ok-modal-content'),
+                    buttons: ['OK']
+                }).present();
+            }).catch((err) => {
+                this.alert.create({
+                    title: this.translate.instant('reset-error-modal-title'),
+                    subTitle: this.translate.instant('reset-error-modal-content') + err,
+                    buttons: ['OK']
+                }).present();
+            });
+        } 
     }
 }
